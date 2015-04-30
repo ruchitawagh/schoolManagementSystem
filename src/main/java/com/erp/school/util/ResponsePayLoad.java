5b45b87e8dda8461c6c0d130041236b446213712
Archana Patil <archana.schoolepr@gmail.com>
2015-04-30 09:28:13 +0530

    Added object in response class.

--- src/main/java/com/erp/school/util/ResponsePayLoad.java
+++ src/main/java/com/erp/school/util/ResponsePayLoad.java
@@ -2,6 +2,15 @@ package com.erp.school.util;
 
 public class ResponsePayLoad {
 	ResponsePayLoadStatus status;
+	private Object object;
+
+	public Object getObject() {
+		return object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
 
 	public ResponsePayLoadStatus getStatus() {
 		return status;
